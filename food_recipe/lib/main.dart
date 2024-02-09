61776d188a47655b1d271e5c14c0cf1c23171fb4
Oluwagbeminiyi Samagbeyi <samagbeyigbeminiyi@gmail.com>
2024-02-09 04:42:33 +0100

    Add appBar widget

--- food_recipe/lib/main.dart
+++ food_recipe/lib/main.dart
@@ -26,8 +26,49 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+        appBar: AppBarSection(),
+        body: Padding(
+          padding: EdgeInsets.only(left: 30.0),
+          child: Column(
+            children: [
+              Text(
+                "How to make french\ntoast",
+                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+              ),
+            ],
+          ),
+        ));
   }
 }
 
+class AppBarSection extends StatelessWidget implements PreferredSizeWidget {
+  const AppBarSection({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        onPressed: () {},
+        icon: Padding(
+          padding: const EdgeInsets.only(left: 20.0),
+          child:
+              Transform.scale(scale: 1.5, child: const Icon(Icons.arrow_back)),
+        ),
+      ),
+      actions: [
+        Padding(
+          padding: const EdgeInsets.only(right: 20.0),
+          child: IconButton(
+            onPressed: () {},
+            icon: Transform.scale(
+                scale: 1.5, child: const Icon(Icons.more_horiz)),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
